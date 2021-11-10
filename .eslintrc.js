4955a0ff96cc520cea100eabd9af624612a150eb
Galiia <>
2021-11-10 10:42:26 +0500

    add configuration

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "import/prefer-default-export": "off",
+    "no-console": "off",
+    "no-alert": "off",
+	'max-len': "off",
+    'default-case': ["error", { "commentPattern": "^skip\\sdefault" }],
+'no-plusplus': ["error", { "allowForLoopAfterthoughts": true }],
+      },
+  plugins: ["jest"],
+};
