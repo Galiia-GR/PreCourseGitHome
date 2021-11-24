4a8940ec2068eeeba7471e4fa2ae6331cf5e538b
Galiia <>
2021-11-24 19:59:45 +0500

    fixed ninth task with tests

--- src/triangle.js
+++ src/triangle.js
@@ -1,5 +1,8 @@
+/* eslint no-return-assign: ["error", "always"] */
 export function triangle(a,b,c) {
     const arr = [a,b,c];
-    const arrTriangle=arr.sort((x,y) => x-y).map((element)=>element*=element);
-    return arrTriangle[2] === arrTriangle[1]+arrTriangle[0] ? "этот треугольник прямоугольный": "этот треугольник непрямоугольный";
+    const arrTriangle=arr.sort((x,y) => x-y).map((value)=>value**2);
+    return arrTriangle[2] === arrTriangle[1]+arrTriangle[0]
+        ? "этот треугольник прямоугольный"
+        : "этот треугольник непрямоугольный";
 }
