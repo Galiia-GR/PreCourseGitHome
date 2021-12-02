591dccb9a4bfeeff24a73bf4f08724df5dd0ff9c
Galiia <>
2021-12-02 13:39:19 +0500

    fixed task seven and add prettier check to package.json

--- src/createParag.js
+++ src/createParag.js
@@ -1,5 +1,5 @@
 export function createParag(element) {
-    element.innerHTML=`<input class ="text"
+  element.innerHTML = `<input class ="text"
        type="text" placeholder="следующий">
 <button class="button">добавить</button>
 <div class="block">
@@ -9,38 +9,37 @@ export function createParag(element) {
     <p>Четвертый параграф</p>
     </div>
 `;
-    const input = element.querySelector(".text");
-    const button = element.querySelector(".button");
-    button.hidden = true;
-    input.addEventListener("input", () => {
-        button.hidden = false;
-    });
-    button.addEventListener("click", buttonClick);
+  const input = element.querySelector(".text");
+  const button = element.querySelector(".button");
+  button.hidden = !input.value;
+  input.addEventListener("input", () => {
+    button.hidden = false;
+  });
+  button.addEventListener("click", buttonClick);
 }
 function buttonClick() {
-    addElementDiv();
-    clearInput();
-    deleteText();
+  addElementDiv();
+  clearInput();
+  deleteText();
 }
 
-function addElementDiv(text) {
-    const input = document.querySelector(".text");
-    const nextEl = document.createElement("p");
-    nextEl.innerHTML = input.value;
-    const block = document.querySelector(".block");
-    block.append(nextEl);
+function addElementDiv() {
+  const input = document.querySelector(".text");
+  const nextEl = document.createElement("p");
+  nextEl.innerHTML = input.value;
+  const block = document.querySelector(".block");
+  block.append(nextEl);
 }
 
- function clearInput() {
- const input = document.querySelector(".text");
- const button = document.querySelector(".button")
-input.value = "";
-button.hidden = true;
+function clearInput() {
+  const input = document.querySelector(".text");
+  const button = document.querySelector(".button");
+  input.value = "";
+  button.hidden = true;
 }
-
 function deleteText() {
-    const el = document.querySelectorAll("p");
-    if (el.length > 5) {
-        el[0].remove();
-    }
+  const el = document.querySelectorAll("p");
+  if (el.length > 5) {
+    el[0].remove();
+  }
 }
